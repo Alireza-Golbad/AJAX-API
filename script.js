14be1d5a8bab4de2fddcd4c440498886a378ca73
Alireza Golbad <63424607+Alireza-Golbad@users.noreply.github.com>
2021-06-23 14:17:06 +0430

    functionalize

--- script.js
+++ script.js
@@ -2,15 +2,16 @@
 const btn = document.querySelector(".btn-country");
 const countriesContainer = document.querySelector(".countries");
 
-const request = new XMLHttpRequest();
-request.open("GET", "https://restcountries.eu/rest/v2/name/iran");
-request.send();
+const getCountryData = function (country) {
+    const request = new XMLHttpRequest();
+    request.open("GET", `https://restcountries.eu/rest/v2/name/${country}`);
+    request.send();
 
-request.addEventListener("load", function () {
-    const [data] = JSON.parse(this.responseText);
-    console.log(data);
+    request.addEventListener("load", function () {
+        const [data] = JSON.parse(this.responseText);
+        console.log(data);
 
-    const html = `
+        const html = `
     <article class="country">
         <img src="${data.flag}" alt="" class="country_img">
         <div class="country_data">
@@ -18,12 +19,17 @@ request.addEventListener("load", function () {
             <h4 class="country_region">${data.region}</h4>
             <p class="country_row"><span>👫</span>${(
                 +data.population / 1000000
-            ).toFixed(1)} people</p>
+            ).toFixed(1)}M people</p>
             <p class="country_row"><span>🗣️</span>${data.languages[0].name}</p>
             <p class="country_row"><span>💰</span>${data.currencies[0].name}</p>
         </div>
     </article> 
     `;
-    countriesContainer.insertAdjacentHTML("beforeend", html);
-    countriesContainer.style.opacity = 1;
-});
+        countriesContainer.insertAdjacentHTML("beforeend", html);
+        countriesContainer.style.opacity = 1;
+    });
+};
+
+getCountryData("iran");
+getCountryData("usa");
+getCountryData("nigeria");
